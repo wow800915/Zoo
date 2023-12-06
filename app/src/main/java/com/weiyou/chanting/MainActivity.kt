7d0df111a7395acdba21e233e868eddf436101a9
Wei-you Chen <>
2023-12-06 11:12:10 +0800

    add navigation

--- app/src/main/java/com/weiyou/chanting/MainActivity.kt
+++ app/src/main/java/com/weiyou/chanting/MainActivity.kt
@@ -10,34 +10,35 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.weiyou.chanting.ui.theme.ChantingTheme
+import androidx.navigation.compose.rememberNavController
+import com.weiyou.chanting.ui.accounts.AccountsScreen
+import com.weiyou.chanting.ui.home.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ChantingTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    INITApp()
                 }
             }
-        }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun INITApp(){
     ChantingTheme {
-        Greeting("Android")
+        val navController = rememberNavController()
+
+        NavHost(navController = navController, startDestination = "homeScreen") {
+            composable("homeScreen") { HomeScreen(navController) }
+            composable("accountsScreen") { AccountsScreen(navController) }
+        }
     }
 }
---
+++ app/src/main/java/com/weiyou/chanting/ui/home/HomeScreen.kt
@@ -0,0 +1,32 @@
+package com.weiyou.chanting.ui.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("HomeScreen")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { navController.navigate("accountsScreen") }) {
+            Text("Go to AccountsScreen")
+        }
+    }
+}
