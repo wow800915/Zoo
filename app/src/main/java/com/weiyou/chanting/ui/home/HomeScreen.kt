0cac0d319db108967352da6e3870a7bbf93ed414
Wei-you Chen <>
2023-12-09 13:00:25 +0800

    add data(from API) from HomeViewModel will update automatically to HomeScreen

--- app/src/main/java/com/weiyou/chanting/ui/home/HomeScreen.kt
+++ app/src/main/java/com/weiyou/chanting/ui/home/HomeScreen.kt
@@ -1,35 +1,75 @@
 package com.weiyou.chanting.ui.home
 
 
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.weiyou.chanting.R
+import com.weiyou.chanting.data.models.NetworkResult
 
 @Composable
 internal fun HomeScreen(homeViewModel: HomeViewModel, navController: NavController) {
 
+    val TAG = "HomeScreen"
 
     LaunchedEffect(true) {
         homeViewModel.getAninalList()
     }
+
+    val aninalList by homeViewModel.aninalList.collectAsState()
 
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
         LazyColumn {
-            items(50) { index ->
-                // Vertical LazyColumn with sample items
-                SampleItem(index)
+            when (aninalList) {
+                is NetworkResult.Success<*> -> {
+                    Log.d("IANIAN", "HomeScreen: Success")
+                    // Render your list items here based on the successful data
+                    items(50) { index ->
+                        SampleItem(index)
+                    }
+                }
+                is NetworkResult.Loading -> {
+                    Log.d("IANIAN", "HomeScreen: Loading")
+                    // Optionally, show a loading indicator
+                    item {
+                        // Your loading indicator UI here
+                        Text(text = "Loading...")
+                    }
+                }
+                is NetworkResult.Error -> {
+                    Log.d("IANIAN", "HomeScreen: Error")
+                    // Optionally, show an error dialog or handle error UI
+                    item {
+                        ErrorAlertDialog((aninalList as NetworkResult.Error).error?.message ?: "Unknown error")
+                    }
+                }
+                else -> {
+                    // Handle other cases if needed
+                }
             }
         }
     }
@@ -38,10 +78,43 @@ internal fun HomeScreen(homeViewModel: HomeViewModel, navController: NavControll
 
 @Composable
 fun SampleItem(index: Int) {
-    Text(
-        text = "Item $index",
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = null, // Provide a meaningful content description
+            modifier = Modifier
+                .size(24.dp) // Adjust the size as needed
+                .padding(end = 8.dp) // Adjust the padding as needed
+        )
+        Text(
+            text = "Item $index",
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun ErrorAlertDialog(error: String) {
+    val context = LocalContext.current
+    AlertDialog(
+        onDismissRequest = { /* Handle dialog dismissal if needed */ },
+        title = { Text(text = "Error") },
+        text = { Text(text = "An error occurred: $error") },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    // Handle button click if needed
+                    Toast.makeText(context, "AlertDialog dismissed", Toast.LENGTH_SHORT).show()
+                }
+            ) {
+                Text(text = "Dismiss")
+            }
+        }
     )
 }
--- app/src/main/java/com/weiyou/chanting/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/weiyou/chanting/ui/home/HomeViewModel.kt
@@ -7,19 +7,24 @@ import androidx.lifecycle.viewModelScope
 import com.weiyou.chanting.data.models.AninalList
 import com.weiyou.chanting.data.models.NetworkResult
 import com.weiyou.chanting.data.repository.HomeRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 
 internal class HomeViewModel(private val homeRepository: HomeRepository) : ViewModel() {
 
-    private val _aninalList  = MutableLiveData<NetworkResult<AninalList>>()
-    val aninalList: LiveData<NetworkResult<AninalList>> get() = _aninalList
+    private val _aninalList = MutableStateFlow<NetworkResult<AninalList>?>(null)
+    val aninalList: StateFlow<NetworkResult<AninalList>?> get() = _aninalList.asStateFlow()
 
     fun getAninalList() {
         viewModelScope.launch {
-                homeRepository.getAninalList().collect { result ->
-                        _aninalList.postValue(result)
-                }
+            _aninalList.value = NetworkResult.Loading // Optional: Set loading state before fetching data
+            homeRepository.getAninalList().collectLatest { result ->
+                _aninalList.value = result
+            }
         }
     }
 
